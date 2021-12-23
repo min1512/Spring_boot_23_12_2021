d3b9f269f27dde0f8d2d0e2c49c15f16b56aabb6
min1512 <min1512@hanmail.net>
2021-12-23 20:20:52 +0900

    create Project

---
+++ demo-4/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+	@GetMapping("/")
+	public ResponseEntity<String> hello() {
+		String msg = "Hello Spring MVC World!";
+		return ResponseEntity.ok(msg);
+	}
+
+}
