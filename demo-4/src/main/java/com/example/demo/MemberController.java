576b524912a8d23a1f647d3447d4bc160aeeb595
min1512 <min1512@hanmail.net>
2022-02-07 22:41:43 +0900

    js / ajax 적용

--- demo-4/src/main/java/com/example/demo/MemberController.java
+++ demo-4/src/main/java/com/example/demo/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -8,10 +9,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.dto.MemberForm;
 import com.example.demo.service.MemberService;
@@ -77,10 +80,38 @@ public class MemberController {
 	  }
 	  
 	  @PostMapping("/signup/ckIdValue")
-	  public String ckIdValue(MemberForm form, @RequestParam Map<String, Object> paramMap ) {
-		  System.out.println(paramMap.get("id").toString());
-		  //memberservice.insertMembers(form);
-		  return "/signup :: #commentTable";
+	  @ResponseBody//이거를 안넣어서... 계속 500에러가 떨어졌네요..
+	  public Map<String,String> ckIdValue(MemberForm form, @RequestParam Map<String, Object> paramMap, Model model ) {
+		  
+		  Map<String,String> map = new HashMap<String,String>();
+		  
+//		  map.put("dog", "강아지");
+//		  map.put("cat", "고양이");
+		  
+		  //System.out.println(paramMap.get("id").toString());
+		  
+		  //model.addAttribute("id", paramMap.get("id").toString() );
+		  
+		  MemberForm member = memberservice.selectById(paramMap.get("id").toString());		  
+		  //System.out.println(member);
+		  
+		  Object memberIdCk = member;
+		  
+		  
+		  map.put("status", "200");  
+		  if(Objects.nonNull(memberIdCk)) {
+			  map.put("ckStatus", "fail");
+			  map.put("message", "이미사용중인 아이디 입니다.");
+			  //model.addAttribute("message","이미사용중인 아이디 입니다.");
+			  //System.out.println("사용 불가");
+		  }else {
+			  map.put("ckStatus", "success");
+			  map.put("message", "사용 가능한 아이디 입니다.");
+			  //model.addAttribute("message","사용 가능한 아이디 입니다.");
+			  //System.out.println("사용 가능");
+		  }
+
+		  return map;
 	  }
   
 }
